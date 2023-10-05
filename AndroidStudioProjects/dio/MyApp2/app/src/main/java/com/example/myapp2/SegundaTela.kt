b5bdde4f0e6e75eb6bce0e7ba145ca193bdb0f89
DiogoSousa80 <sousadiogo453@gmail.com>
2023-10-04 21:52:27 -0300

    Criando strings de alguns pais selecionadas

---
+++ AndroidStudioProjects/dio/MyApp2/app/src/main/java/com/example/myapp2/SegundaTela.kt
@@ -0,0 +1,15 @@
+package com.example.myapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaTela : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_tela)
+    }
+}
